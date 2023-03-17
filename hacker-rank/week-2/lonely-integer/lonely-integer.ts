4e46b8319aae5bf2cdc9966631ba93695b5c0512
Belkross <belkross.pro@gmail.com>
2023-03-17 22:56:12 +0100

    refactor: improve naming

--- hacker-rank/week-2/lonely-integer/lonely-integer.ts
+++ hacker-rank/week-2/lonely-integer/lonely-integer.ts
@@ -3,9 +3,9 @@ function lonelyinteger(integers: number[]): number {
 
 	for (let i = 0; i < integers.length; i += 2) {
 		const isLastElement = i === integers.length - 1
-		const occursOnce = integers[i] !== integers[i + 1]
+		const ḑifferentFromNext = integers[i] !== integers[i + 1]
 
-		if (isLastElement || occursOnce) return integers[i]
+		if (isLastElement || ḑifferentFromNext) return integers[i]
 		else continue
 	}
 
