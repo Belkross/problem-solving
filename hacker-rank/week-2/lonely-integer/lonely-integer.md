bc0bf9683e995bd34c5a9d6e3a42947021dfc9b1
Belkross <belkross.pro@gmail.com>
2023-03-22 19:41:18 +0100

    refactor: improve the logic readability

--- hacker-rank/week-2/lonely-integer/lonely-integer.md
+++ hacker-rank/week-2/lonely-integer/lonely-integer.md
@@ -1,11 +1,13 @@
     find the unique element that occurs once in an array
 
     INTEGERS: integer[]
+    output: number
 
     #start
       sort all the elements of the array in ascending order
       from i=0 to i<INTEGERS.length progressing i+2
         if INTEGERS[i] is the last element OR INTEGERS[i] != INTEGERS[i+1]
-          write INTEGERS[i]
+          output <- INTEGERS[i]
+          break
         else continue
     #end
--- hacker-rank/week-2/lonely-integer/lonely-integer.ts
+++ hacker-rank/week-2/lonely-integer/lonely-integer.ts
@@ -1,13 +1,17 @@
 function lonelyinteger(integers: number[]): number {
+	let output = NaN
+
 	integers.sort((a, b) => a - b)
 
 	for (let i = 0; i < integers.length; i += 2) {
 		const isLastElement = i === integers.length - 1
-		const ḑifferentFromNext = integers[i] !== integers[i + 1]
+		const differentFromNext = integers[i] !== integers[i + 1]
 
-		if (isLastElement || ḑifferentFromNext) return integers[i]
-		else continue
+		if (isLastElement || differentFromNext) {
+			output = integers[i]
+			break
+		} else continue
 	}
 
-	return NaN //should never happen since the inputs are always valid
+	return output
 }
