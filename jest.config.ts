0b84657c9e5396560b206195be136ffc22168fc1
belkross <belkross.pro@gmail.com>
2024-04-03 18:53:01 +0200

    build: setup test runner

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "jest"
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+}
+
+export default config
